8aff29664cd67d2d54365576bf5ca5b30064fb22
Yukii <alexisperignon@live.FR>
2019-05-23 21:25:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,24 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+//const config_tok = require("./config_noshare.json"); // Private config in which you can specify your token if you want to use local way
 var webshot = require('webshot');
-/*var fs      = require('fs');
+var fs = require('fs');
 var options = {
+    shotOffset: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
+    },
     shotSize: {
         width: 'all',
-        height: 'all'
+        height: 1100
     },
-    siteType: 'url'
-};*/
+    siteType: 'url',
+
+};
+
 client.on("message", async message => {
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
@@ -27,22 +36,48 @@ client.on("message", async message => {
         message.delete().catch(O_o => {});
         message.channel.send(sayMessage);
     }
-    if (command === "img") {
-        message.channel.send("hey");
-        /*webshot('https://u.gg/lol/champions/nidalee/build/?role=jungle', 'nidalee.png', options, function (err) {
-            if (!err) {
-                console.log('Screenshot taken!');
-            }
-        });*/
-        /*message.channel.send('cc', {
-            files: [
-                "./nidalee.png"
-            ]
-        });*/
-        //fs.unlinkSync("./nidalee.png");
+
+    if (command === "hero") {
+        //fs.unlinkSync('./nidalee.png');
+        //fs.unlinkSync('./nidalee2.png');
+        //fs.unlinkSync('./nidalee3.png');
+        if (args[1] != 'middle' && args[1] != 'support' && args[1] != 'top' && args[1] != 'adc' && args[1] != 'jungle' || args.length > 2) {
+            message.channel.send("Command line is !a hero <champion> <middle/top/jungle/adc/top");
+        } else {
+
+            webshot('https://u.gg/lol/champions/' + args[0] + '/build/?role=' + args[1], 'nidalee.png', options, function (err) {
+                if (!err) {
+                    console.log('Screenshot taken!');
+                    message.channel.send("", {
+                        files: ['./nidalee.png']
+                    }).then().catch(console.error);
+                }
+            });
+            options.shotOffset.top += 1000;
+            webshot('https://u.gg/lol/champions/' + args[0] + '/build/?role=' + args[1], 'nidalee2.png', options, function (err) {
+                if (!err) {
+                    console.log('Screenshot taken!');
+                    message.channel.send("", {
+                        files: ['./nidalee2.png']
+                    }).then().catch(console.error);
+                }
+            });
+            options.shotOffset.top += 1000;
+            webshot('https://u.gg/lol/champions/' + args[0] + '/build/?role=' + args[1], 'nidalee3.png', options, function (err) {
+                if (!err) {
+                    console.log('Screenshot taken!');
+                    message.channel.send("", {
+                        files: ['./nidalee3.png']
+                    }).then().catch(console.error);
+                }
+            });
+            options.shotOffset.top = 0;
+
+        }
+
     }
 
 });
 
-//client.login(config.token);
-client.login(process.env.TOKEN);
+//client.login(config_tok.token); //Local way
+client.login(process.env.TOKEN); //Heroku way
