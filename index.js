d3372b37001cfc4bdfb60c42a98a7b0c6e1a1f22
Yukii <alexisperignon@live.fr>
2019-05-25 13:37:25 +0200

    test unescape

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ client.on("message", async message => {
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+    const command = unescape(args.shift().toLowerCase());
     if (command === "ping") {
         const m = await message.channel.send("Ping?");
         m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
