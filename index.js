086510ee1ba29be7481a2f0b25805ffd8df72d1d
Yukii <alexisperignon@live.fr>
2019-05-26 11:56:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function getChampFromKey(message,key){
           // skip loop if the property is from prototype
           if (!champion.data.hasOwnProperty(name)) continue;
           var obj = champion.data[name];
-          message.channel.send("act2 ; " key + " :"  +obj.key);
+          //message.channel.send("act2 ; " key + " :"  +obj.key);
           if(obj.key === key){message.channel.send("act ; " + name); return name;}
       }
       return "Not Found";
