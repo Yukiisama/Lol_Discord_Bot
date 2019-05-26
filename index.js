624e3591bce3e2580f6753500a9505b0025bb2bd
Yukii <alexisperignon@live.fr>
2019-05-26 11:10:04 +0200

    debug

--- index.js
+++ index.js
@@ -138,8 +138,8 @@ client.on("message", async message => {
         			/*bannedChampionsstring[key] = "Champ "+key+" : pickTurn : "+data.bannedChampions[key].pickTurn
         											+" \n champion : "+getChampFromId(data.bannedChampions[key].championId)
                                                     +" \n teamId : "+data.bannedChampions[key].teamId;*/
-                    message.channel.send("\n"+key+ " : " +getChampFromId(data.bannedChampions[key].championId ));
-                    console.log("\n"+key+ " : " +getChampFromId(data.bannedChampions[key].championId) );
+                    //message.channel.send("\n"+key+ " : " +getChampFromId(data.bannedChampions[key].championId ));
+                    console.log("\n"+key+ " : " +data.bannedChampions[key].championId);
         		}
         		 
             	message.channel.send("game Start time : " +data.gameStartTime
